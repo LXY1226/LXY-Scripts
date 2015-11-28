235e98ef45c21f986abc673cb0c288506ea82eaa
lxy1226 <767763591@qq.com>
2015-11-28 11:32:55 +0800

    更新脚本

--- waifu.sh
+++ waifu.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+##  _LXY_'s Scripts
+##  Waifu2x 图片处理:github/nagadomi/waifu2x
+##  此处使用的是 github/WL-Amigo/waifu2x-converter-cpp 的改进版。
+##  很简陋的一个实用的小程序~
 for i in old/*;
 do
 	echo -e "\e[1;36m$i\e[0m"
